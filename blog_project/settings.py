5988d637a15ed3d56cd334edf1e9b590bf14e975
Shenjinsa19 <shenjinsolomon@gmail.com>
2025-04-28 12:33:21 +0530

    settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -190,7 +189,6 @@ ALLOWED_HOSTS = ['blog-project32.onrender.com', 'localhost', '127.0.0.1']
 
 # Static files settings
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
