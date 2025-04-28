95f46c26f135042176d6b9b383120fd5bda9d3aa
Shenjinsa19 <shenjinsolomon@gmail.com>
2025-04-28 11:21:40 +0530

    Fix static files settings and CSRF trusted origins

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from blog import views  
 from .views import createview,detailview
 
@@ -7,4 +9,4 @@ urlpatterns = [
     path('detail/<int:pk>/', detailview.as_view(), name='post-detail'),
     path('register/', views.register.as_view(), name='register'),
     path('login/', views.login.as_view(), name='login'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
