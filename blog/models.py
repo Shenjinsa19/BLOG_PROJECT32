c7fc7fa34ec564374f1070b74e696a1256cab9e7
Shenjinsa19 <shenjinsolomon@gmail.com>
2025-04-29 15:52:01 +0530

    Successfully set up and tested Django project

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ class Post(models.Model):
     
     title = models.CharField(max_length=100)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
     created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
