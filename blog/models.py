057dce93fb9260d3c16efc7971e2fcccd70fa114
Shenjinsa19 <shenjinsolomon@gmail.com>
2025-04-29 16:37:34 +0530

    Successfully set up and tested Django project

--- blog/models.py
+++ blog/models.py
@@ -10,3 +10,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+ 
+    def __str__(self):
+        return self.user.username
