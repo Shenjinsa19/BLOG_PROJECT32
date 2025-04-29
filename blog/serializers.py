2f894404553bbc557e8cafbf3980425ec6265875
Shenjinsa19 <shenjinsolomon@gmail.com>
2025-04-29 12:17:37 +0530

    Successfully set up and tested Django project

--- blog/serializers.py
+++ blog/serializers.py
@@ -6,7 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
         model = myusers
         fields = ['id','title','content','author','created_at']
         read_only_fields = ['author','created_at']
-from rest_framework import serializers
+
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
