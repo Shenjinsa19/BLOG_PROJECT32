18adc8e62096a894113711731ded696fff351153
Shenjinsa19 <shenjinsolomon@gmail.com>
2025-05-05 16:59:47 +0530

    Successfully set up and tested Django project

--- blog/urls.py
+++ blog/urls.py
@@ -20,37 +20,3 @@ urlpatterns = [
     path('comments/<int:pk>/', CommentDetailWithRepliesView.as_view(), name='comment-detail-with-replies')#cmt ku vanta response
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# from django.urls import path
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from .views import (
-#     PostListCreateView,
-#     PostDetailView,
-#     UserListView,
-#     RegisterView,
-#     LoginView,
-#     CategoryListCreateView,
-#     like_post,
-#     dislike_post,
-#     my_disliked_posts,
-#     CommentListCreateView,
-#     CommentLikeView,
-#     CommentDetailWithRepliesView,
-#     CommentDislikeView  # Ensure this import is correct
-# )
-
-# urlpatterns = [
-#     path('posts/', PostListCreateView.as_view(), name='post-list-create'),  # Create post
-#     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # Post view
-#     path('register/', RegisterView.as_view(), name='register'),
-#     path('login/', LoginView.as_view(), name='login'),
-#     path('users/', UserListView.as_view(), name='user-list'),
-#     path('category/', CategoryListCreateView.as_view(), name='category-list'),
-#     path('posts/<int:post_id>/like/', like_post, name='like-post'),  # Like a post
-#     path('posts/<int:post_id>/dislike/', dislike_post, name='dislike-post'),  # Dislike a post
-#     path('posts/disliked/', my_disliked_posts, name='my-disliked-posts'),  # See what I disliked
-#     path('posts/<int:post_id>/comments/', CommentListCreateView.as_view(), name='post-comments'),  # Comments on post
-#     path('comments/<int:comment_id>/like/', CommentLikeView.as_view(), name='comment-like'),  # Like comment
-#     path('comments/<int:comment_id>/dislike/', CommentDislikeView.as_view(), name='comment-dislike'),  # Dislike comment
-#     path('comments/<int:pk>/', CommentDetailWithRepliesView.as_view(), name='comment-detail-with-replies')  # Comment with replies
-# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
